618f372f295fd06f9ff019fbedd8e8aab36fb05d
Lucas Baizer <titaniumsapphire32@gmail.com>
2023-05-15 22:51:52 -0700

    Added autoclean

--- all.sh
+++ all.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
+rm ./*.ll
+rm ./*.o
+rm ./exec
 cargo run -- --emit llvm-ir,object ../keid/assets/core/*.keid ../keid/assets/core/**/*.keid ../keid/assets/std/*.keid ../keid/assets/impl/std.linux.keid ../keid/assets/compiler/main.keid
