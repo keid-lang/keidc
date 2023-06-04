b32b23c36f686aeb815d7b2c85074c1c6eef4535
Lucas Baizer <titaniumsapphire32@gmail.com>
2023-06-04 13:47:05 -0700

    Renamed primitives

--- all.sh
+++ all.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
+
 rm ./*.ll
 rm ./*.o
 rm ./exec
+
+export GLOBIGNORE="String.test.keid:StringBuilder.test.keid::array.test.keid:Class.test.keid"
 cargo run -- --emit llvm-ir,object ../keid/assets/core/*.keid ../keid/assets/core/**/*.keid ../keid/assets/std/*.keid ../keid/assets/impl/std.linux.keid ../keid/assets/compiler/main.keid
+unset GLOBIGNORE
