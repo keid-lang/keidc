8859121346ca259f8b1326f2e569d453d37505f9
Lucas Baizer <titaniumsapphire32@gmail.com>
2023-06-09 13:22:41 -0700

    Changed impl dirs

--- all.sh
+++ all.sh
@@ -4,6 +4,5 @@ rm ./*.ll
 rm ./*.o
 rm ./exec
 
-export GLOBIGNORE="String.test.keid:StringBuilder.test.keid::array.test.keid:Class.test.keid"
-cargo run -- --emit llvm-ir,object ../keid/assets/core/*.keid ../keid/assets/core/**/*.keid ../keid/assets/std/*.keid ../keid/assets/impl/std.linux.keid ../keid/assets/compiler/main.keid
-unset GLOBIGNORE
+cargo run -- --emit llvm-ir,object ../keid/assets/core/*.keid ../keid/assets/core/**/*.keid ../keid/assets/std/*.keid ../keid/assets/impl/linux/**/*.keid ../keid/assets/compiler/main.keid
+ 
