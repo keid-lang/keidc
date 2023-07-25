ac804d50f6db7754bb855a9653269363fa3614cc
Lucas Baizer <titaniumsapphire32@gmail.com>
2023-07-24 20:22:50 -0700

    Less stupid stuff

--- all.sh
+++ all.sh
@@ -5,4 +5,4 @@ rm ./*.o
 rm ./exec
 
 cd ../keid/rtdbg ; cargo build --release ; cd ../../keidc ; cp ../keid/rtdbg/target/release/librtdbg.a .
-cargo run -- --emit llvm-ir,object ../keid/assets/core/*.keid ../keid/assets/core/**/*.keid ../keid/assets/std/*.keid ../keid/assets/impl/linux/**/*.keid ../keid/assets/compiler/main.keid
+cargo run -- --emit llvm-ir,object ../keid/assets/core/*.keid ../keid/assets/core/**/*.keid ../keid/assets/std/*.keid ../keid/assets/impl/linux/**/*.keid ../keid/assets/compiler/*.keid
