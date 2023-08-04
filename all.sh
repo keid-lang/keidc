1a4f239176af37d669dbca4c90a336b47055fa00
Lucas Baizer <titaniumsapphire32@gmail.com>
2023-08-03 19:14:26 -0700

    Changes

--- all.sh
+++ all.sh
@@ -2,7 +2,9 @@
 
 rm ./*.ll
 rm ./*.o
+rm ./*.kpkg
 rm ./exec
 
 cd ../keid/rtdbg ; cargo build --release ; cd ../../keidc ; cp ../keid/rtdbg/target/release/librtdbg.a .
-cargo run -- --emit llvm-ir,object ../keid/assets/core/*.keid ../keid/assets/core/**/*.keid ../keid/assets/std/*.keid ../keid/assets/impl/linux/**/*.keid ../keid/assets/compiler/*.keid
+cargo build --release
+./target/release/keidc -- --emit llvm-ir,object ../keid/assets/core/*.keid ../keid/assets/core/**/*.keid ../keid/assets/std/*.keid ../keid/assets/impl/linux/**/*.keid ../keid/assets/compiler/*.keid ../keid/assets/compiler/**/*.keid
