7e5d89efb8c9e23ed45e8d0d0b592acda4d7b1f5
Lucas Baizer <titaniumsapphire32@gmail.com>
2023-08-21 09:26:53 -0700

    Continued work on the KSDL parser

--- all.sh
+++ all.sh
@@ -20,5 +20,6 @@ cd ../keid/rtdbg
 cargo build --release
 cd ../../keidc
 cp ../keid/rtdbg/target/release/librtdbg.a .
+cp $(find $LLVM_SYS_150_PREFIX -name libunwind.a) .
 
 ./target/$MODE/keidc --target x86_64-unknown-linux-gnu --emit llvm-ir,object ../keid/assets/core/*.keid ../keid/assets/core/**/*.keid ../keid/assets/std/*.keid ../keid/assets/impl/linux/**/*.keid ../keid/assets/compiler/*.keid ../keid/assets/compiler/**/*.keid
